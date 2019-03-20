3981eabaad8005dbd655eaedbf33417bf12155d7
cristianladoglz <cris.1998@hotmail.es>
2019-03-20 16:07:00 +0100

    A little bug arranged

--- test/test.js
+++ test/test.js
@@ -7,11 +7,11 @@ describe('Simple test', function () {
   it('2 + 2 = 4', function () {
     assert.equal(2+2, 4);
   })
+})
   
 describe('Semantic chat constructor', function () {
   it('getUrl and getInterlocutorWebId tests', function () {
     assert.equal(chat.getUrl(), "http://prueba:8080");
     assert.equal(chat.getInterlocutorWebId(), 123);
   })
-
 })
