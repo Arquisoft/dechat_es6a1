88c59ffa975b00d244a5bfb99f867b5eff4a610c
cristianladoglz <cris.1998@hotmail.es>
2019-03-20 16:00:38 +0100

    Test semantic chat constructor

--- test/test.js
+++ test/test.js
@@ -1,9 +1,17 @@
 require('chai');
 var assert = require('assert');
+const SemanticChat = require('../src/lib/semanticchat');
+const chat = new SemanticChat({ url: "http://prueba:8080", chatBaseUrl: "hola", interlocutorWebId: 123 });
 
 describe('Simple test', function () {
-  it('2 plus 2 is 4', function () {
-    assert.equal(2+2, 4)
+  it('2 + 2 = 4', function () {
+    assert.equal(2+2, 4);
+  })
+  
+describe('Semantic chat constructor', function () {
+  it('getUrl and getInterlocutorWebId tests', function () {
+    assert.equal(chat.getUrl(), "http://prueba:8080");
+    assert.equal(chat.getInterlocutorWebId(), 123);
   })
 
 })
