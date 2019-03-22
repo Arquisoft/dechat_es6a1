e7e3c1cda3e0d2140d8d66650d1c8e4c0d8e6d44
cristianladoglz <cris.1998@hotmail.es>
2019-03-22 18:37:50 +0100

    Fixed loader tests and added core tests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,15 @@
 require('chai');
 var assert = require('assert');
+const auth = require('solid-auth-client');
+
 const SemanticChat = require('../src/lib/semanticchat');
 const chat = new SemanticChat({ url: "http://prueba:8080", chatBaseUrl: "hola", interlocutorWebId: 123 });
 
 const Loaderjs = require('../src/lib/loader');
-const loader = new Loaderjs("hola");
+const loader = new Loaderjs(auth.fetch);
+
+const DeChatCore = require('../src/lib/core');
+const core = new DeChatCore(auth.fetch);
 
 describe('Simple test', function () {
   it('2 + 2 = 4', function () {
@@ -28,9 +33,28 @@ describe('Semantic chat constructor', function () {
     assert.equal(chat.getMessages()[1], "Adiós");
   })
   
-    it('loadMessageFromURL', function () {
-      assert.equal(loader.loadFromUrl(chat.getUrl(), 123, "hola").getUrl(), "http://prueba:8080");
-      assert.equal(loader.loadFromUrl(chat.getUrl(), 123, "hola").getInterlocutorWebId(), 123);
-      assert.equal(loader.loadFromUrl(chat.getUrl(), 123, "hola").getMessages().length, 0);
-  })
+})
+
+describe('Loader tests', function () {
+	it('loadMessageFromURL', async function () {
+		let s= await loader.loadFromUrl("https://testdechat6a1.solid.community/profile/card#me", 123, "hola");
+		assert.equal(s.getUrl(), "https://testdechat6a1.solid.community/profile/card#me");
+		assert.equal(s.userWebId, 123);
+		assert.equal(s.getMessages().length, 0);
+	})
+	
+})
+
+describe('Core tests', function () {
+	it('core constructor', function () {
+		assert.equal(core.alreadyCheckedResources.length, 0);
+		assert.equal(core.logger.level, 'error');
+		console.log(core.getFormattedName("https://testdechat6a1.solid.community/profile/card#me"));
+	})
+  
+	it('getFormattedName', async function () {
+		let s= await core.getFormattedName("https://testdechat6a1.solid.community/profile/card#me");
+		assert.equal(s, "Tests Dechat_6a1");
+	})
+  
 })
