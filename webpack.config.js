906831ba579d9078f5fd74cbb6c3d6d27bfbca77
isafdezpe <isafdezpe98@gmail.com>
2019-03-26 14:34:26 +0100

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   entry: './src/main/index.js',
@@ -15,4 +15,4 @@ module.exports = {
     '@trust/webcrypto': 'crypto',
     'fs': 'empty'
   }
-};
+}
