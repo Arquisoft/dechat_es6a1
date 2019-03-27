26966f31a591d418569a924b10e87dd0bbb03f7e
isafdezpe <isafdezpe98@gmail.com>
2019-03-27 18:15:44 +0100

    minimal changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/main/index.js',
+  entry: './src/main/app.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'src/main/dist')
