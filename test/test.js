bfe438444b58f8d6bb06c236b49c503c660758ba
cristianladoglz <cris.1998@hotmail.es>
2019-03-30 09:53:23 +0100

    Added core tests

--- test/test.js
+++ test/test.js
@@ -13,6 +13,9 @@ const loader = new Loaderjs(auth.fetch);
 const DeChatCore = require('../src/lib/core');
 const core = new DeChatCore(auth.fetch);
 
+const DataSync = require('../src/lib/datasync');
+const dataSync = new DataSync(auth.fetch);
+
 describe('Simple test', function () {
   it('2 + 2 = 4', function () {
     assert.equal(2+2, 4);
@@ -44,11 +47,6 @@ describe('Loader tests', function () {
 		assert.equal(s.userWebId, 123);
 		assert.equal(s.getMessages().length, 0);
 	})
-	it('loadFromURL', async function () {
-		let chat = loader.loadFromUrl("https://alvarogonzalezcarracedo2.solid.community/private/dechat_201903291235.ttl","https://alvarogonzalezcarracedo2.solid.community/profile/card#me","https://alvarogonzalezcarracedo2.solid.community/private/dechat_201903291235.ttl");
-		const name = await core.getFormattedName(chat.userWebId);
-        	assert.equal(name, 'Alvaro');
-	})
 	
 })
 
@@ -56,27 +54,46 @@ describe('Core tests', function () {
 	it('core constructor', function () {
 		assert.equal(core.alreadyCheckedResources.length, 0);
 		assert.equal(core.logger.level, 'error');
-		console.log(core.getFormattedName("https://testdechat6a1.solid.community/profile/card#me"));
 	})
   
 	it('getFormattedName', async function () {
 		let s= await core.getFormattedName("https://testdechat6a1.solid.community/profile/card#me");
 		assert.equal(s, "Tests Dechat_6a1");
 	})
-	/*
-	it('setUpNewChat(No funciona)', async function () {
+	
+	it('setUpNewChat', async function () {
 		let s= await core.setUpNewChat ("https://testdechat6a1.solid.community/profile/card",150,151);
-		assert.equal(s, new SemanticChat({ url: "https://testdechat6a1.solid.community/profile/card#me", userWebId : 150, chatBaseUrl: "https://testdechat6a1.solid.community/profile/card", interlocutorWebId: 151 }););
+		assert(s.getUrl().includes("https://testdechat6a1.solid.community/profile/card#"));
+		assert.equal(s.chatBaseUrl, undefined);
+		assert.equal(s.userWebId, 150);
+		assert.equal(s.interlocutorWebId, 151);
+		assert.equal(s.numberOfMessages, 0);
 	})
 	
-	it('generateUniqueUrlForResource ', async function () {
+	it('generateUniqueUrlForResource', async function () {
 		let s= await core.generateUniqueUrlForResource("https://alvarogonzalezcarracedo2.solid.community/profile/card");
-		assert.equal(s,"https://alvarogonzalezcarracedo2.solid.community/profile/card#me");
+		assert(s.includes("https://alvarogonzalezcarracedo2.solid.community/profile/card#"));
 	})
 	
-	it('joinExistingChat(NO FUnciona)', async function () {
-		let s= await core.joinExistingChat  ("https://testdechat6a1.solid.community/profile/card#me", "https://alvarogonzalezcarracedo2.solid.community/profile/card#me", "https://testdechat6a1.solid.community/profile/card#me", "https://alvarogonzalezcarracedo2.solid.community/private");
+	it('joinExistingChat', async function () {
+		const file=dataSync.createEmptyFileForUser("https://testdechat6a1.solid.community/private/test");
+		let s= await core.joinExistingChat("https://testdechat6a1.solid.community/profile/card#me", "https://alvarogonzalezcarracedo2.solid.community/profile/card#me", "https://testdechat6a1.solid.community/profile/card#me", "https://alvarogonzalezcarracedo2.solid.community/private",
+		dataSync, "https://testdechat6a1.solid.community/private/test");
+		assert(s.includes("https://alvarogonzalezcarracedo2.solid.community/private#"));
 	})
-	*/
+	
+	it('generateResponseToInvitation', async function () {
+		let s= await core.generateResponseToInvitation("https://testdechat6a1.solid.community/profile/card#me", "https://alvarogonzalezcarracedo2.solid.community/invitation",
+		"https://testdechat6a1.solid.community/profile/card#me",
+		"https://alvarogonzalezcarracedo2.solid.community/profile/card#me","no");
+		assert(s.notification.includes("https://alvarogonzalezcarracedo2.solid.community/invitation"));
+		assert(s.notification.includes("https://testdechat6a1.solid.community/profile/card#"));
+		assert(s.sparqlUpdate.includes("http://schema.org/RsvpResponseNo"));
+	})
+	
+	it('getInboxUrl', async function () {
+		let s= await core.getInboxUrl("https://testdechat6a1.solid.community/profile/card#me");
+		assert.equal(s, "https://testdechat6a1.solid.community/inbox/");
+	})
   
 })
