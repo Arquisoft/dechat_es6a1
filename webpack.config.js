b15941c228f2e3a358c526dda4c53847fb6b552f
cristianladoglz <cris.1998@hotmail.es>
2019-04-03 16:16:30 +0200

    Documentation into docs/docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/main/app.js',
+  entry: './src/app.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'src/main/dist')
